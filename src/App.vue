942385fc67bb08abb09ffbbced8bcf3244f5796a
Kevin Artha Wijaya <kevinarthaw@gmail.com>
2025-12-31 00:23:21 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="min-h-screen flex items-center justify-center">
+    <h1 class="text-3xl font-bold underline italic">
+      Vue + Tailwind is working ✅
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
